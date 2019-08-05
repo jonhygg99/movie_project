e888e6e3efaf9abb278f2b8c4b0e48229605f381
Jonathan Gómez <jonathan.gomez@tomtom.com>
2019-08-05 17:40:05 +0200

    second

--- movie_proj/src/App.js
+++ movie_proj/src/App.js
@@ -2,12 +2,25 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const API_KEY = 'a2fd13908e7ca798960594a0d61175e6'
+const API_POPULAR = 'https://api.themoviedb.org/3/movie/popular?api_key=' + API_KEY
+
 class App extends React.Component {
   constructor(props) {
     super(props);
-			this.state = { userInput: '' };
+			this.state = { 
+        userInput: '',
+        data_movie: []
+      };
    		this.handleUserInput = this.handleUserInput.bind(this);
   }
+
+  componentDidMount() {
+    fetch(API_POPULAR)
+    .then(response => response.json())
+    .then(data => this.setState({ data_movie: data.data_movie }));
+  }
+  
 
   handleUserInput(e) {
     this.setState({
@@ -23,14 +36,18 @@ class App extends React.Component {
         <h1>Movie Database</h1>
       </header>
       <body className="App-body">
-       <input type="text" placeholder="Search movies..."  
+       <input className="Search_bar" type="text" placeholder="Search movies..."  
           onChange={this.handleUserInput}
           value={this.state.userInput}/>
         <h1>{this.state.userInput}</h1>
        <p>Search for something new</p>
+       <p>{API_POPULAR}</p>
       </body>
       <footer className="App-footer">
+        <div>
         <p>Lorem ipsum dolor sit amet consectetur, adipiscing elit ornare nisl.</p>
+        </div>
+
       </footer>
     </div>
   );
